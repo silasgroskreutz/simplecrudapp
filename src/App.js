a69a05823ee263e12c10860a126e261712545a75
Silas Groskreutz <silasgroskreutz@gmail.com>
2019-10-13 16:28:35 -0500

    Committing practice App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ProductItem from './ProductItem';
+import AddProduct from './AddItem';
+
+const products = [
+  {
+    name: 'ipad',
+    price: 200
+},
+  {
+    name: 'iphone',
+    price: 650
+  }
+];
+
+localStorage.setItem('products', JSON.stringify(products));
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: JSON.parse(localStorage.getItem('products'))
+    };
+
+    this.onAdd = this.onAdd.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+    this.onEditSubmit = this.onEditSubmit.bind(this);
+  }
+
+  componentWillMount() {
+    const products = this.getProducts();
+
+    this.setState({ products });
+  }
+
+  getProducts() {
+    return this.state.products
+  }
+
+  onAdd(name, price) {
+    const products = this.getProducts();
+
+    products.push({
+      name,
+      price
+    });
+
+    this.setState({ products });
+  }
+
+  onDelete(name) {
+    const products = this.getProducts();
+
+    const filteredProducts = products.filter(product => {
+      return product.name !== name;
+    });
+
+    this.setState({ products: filteredProducts });
+
+  }
+
+  onEditSubmit(name, price, originalName) {
+    let products = this.getProducts();
+
+    products = products.map(product => {
+      if (product.name === originalName) {
+        product.name = name;
+        product.price = price;
+      }
+
+      return product;
+    });
+
+    this.setState({ products });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Products Manager</h1>
+
+        <AddProduct
+          onAdd={this.onAdd}
+        />
+
+        {
+          this.state.products.map(product => {
+            return (
+              <ProductItem
+                key={product.name}
+                {...product}
+                onDelete={this.onDelete}
+                onEditSubmit={this.onEditSubmit}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
